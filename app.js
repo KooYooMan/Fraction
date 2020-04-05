0519bad844fbe0d3311fc7419ba87b83c090fc26
Le Duy <leqduyvp@gmail.com>
2020-04-05 19:38:32 +0700

    feat: add homepage

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/', (req, res)=> {
+    res.sendFile(__dirname + '/public/stage1.html');
+})
+
 app.listen(3000, () => {
     console.log(3000);
     
