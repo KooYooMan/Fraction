0684480c07b42464dc935d032325dd6d02ec474d
Le Duy <leqduyvp@gmail.com>
2020-05-05 13:35:06 +0700

    Updates

---
+++ precache-manifest.0bcfc9c46095dfd88aa76cb540c22d8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e0244bef83a17eaa013e6b26667151",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3477418b5f8511144cef",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "a64c4742043046d08bc5",
+    "url": "/static/css/main.81a443d7.chunk.css"
+  },
+  {
+    "revision": "3477418b5f8511144cef",
+    "url": "/static/js/2.9525ebbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9525ebbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64c4742043046d08bc5",
+    "url": "/static/js/main.fb5896d2.chunk.js"
+  },
+  {
+    "revision": "49a9cd60a691b5ebfcd5",
+    "url": "/static/js/runtime-main.4cebe7fb.js"
+  }
+]);
